890cd303bd48e3d82582304f69986469eea09fa4
Ajay Kumar Jha <134692366+Ajaygenuinedoubt@users.noreply.github.com>
2025-03-04 16:16:00 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-!pip install streamlit pandas numpy requests scikit-learn matplotlib seaborn
+pip install streamlit pandas numpy requests scikit-learn matplotlib seaborn
 
 import streamlit as st
 import pandas as pd
